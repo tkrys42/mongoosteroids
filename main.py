c4a72b897e298369fea1fc171e36abd57c5c405a
tkrys42 <tomkrystinik@gmail.com>
2026-03-19 07:25:43 -0500

    I: shots destroy asteroids

--- main.py
+++ main.py
@@ -42,6 +42,12 @@ def main():
                 log_event("player_hit")
                 print("Game over!")
                 sys.exit()
+        for obj in asteroids:
+            for shot in shots:
+                if obj.collides_with(shot):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    obj.kill()
         for obj in drawable:
             obj.draw(screen)
         pygame.display.flip()
