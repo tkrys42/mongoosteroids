ee1c0117558caa05d480f4de6764fd40a5c8d596
tkrys42 <tomkrystinik@gmail.com>
2026-03-29 07:49:40 -0500

    K: Added scoring system

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from shot import Shot
 
 def main():
     pygame.init()
+    pygame.font.init()
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -30,6 +31,9 @@ def main():
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     asteroid_field = AsteroidField()
 
+    score = 0
+    font = pygame.font.Font(None, 36)
+
 # game loop
     while True:
         log_state()
@@ -42,16 +46,27 @@ def main():
             if obj.collides_with(player):
                 log_event("player_hit")
                 print("Game over!")
+                print(f"Final Score: {score}!")
                 sys.exit()
         # when shot, the shot is destroyed and the asteroid splits or is destroyed if small enough
-        for obj in asteroids:
+        for asteroid in asteroids:
             for shot in shots:
-                if obj.collides_with(shot):
+                if asteroid.collides_with(shot):
                     log_event("asteroid_shot")
                     shot.kill()
-                    obj.split()
+                    asteroid.split()
+                    if asteroid.radius < 30:
+                        score += 250
+                    elif asteroid.radius < 50:
+                        score += 150
+                    else:
+                        score += 100
+
         for obj in drawable:
             obj.draw(screen)
+        # Render the score on the screen
+        score_surface = font.render(f"Score: {score}", True, "white")
+        screen.blit(score_surface, (10, 10))
         # refresh the screen, called last
         pygame.display.flip()
         milliseconds = clock.tick(60)
